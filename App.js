8cd936beb34bed623995f1cd331068a863bc0de4
johnpmitsch <johnpmitsch@gmail.com>
2017-12-29 11:22:20 -0500

    added react native elements

--- App.js
+++ App.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Header } from 'react-native-elements';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+				<View>
+          <Header
+            leftComponent={{ icon: 'menu', color: '#fff' }}
+            centerComponent={{ text: 'Cryptorganize', style: { color: '#fff' } }}
+            rightComponent={{ icon: 'home', color: '#fff' }}
+          />
+					<Text>Open up App.js to start working on your app!</Text>
+					<Text>Changes you make will automatically reload.</Text>
+					<Text>Shake your phone to open the developer menu.</Text>
+				</View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
