e9abca5f08bce1bbd5d80523e69a9c24c22bf9c0
johnpmitsch <johnpmitsch@gmail.com>
2017-12-31 18:32:44 -0500

    updated test helper

--- test/App.test.js
+++ test/App.test.js
@@ -1,7 +1,6 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import App from "../App";
-import "./helpers/testSetup";
 
 it("renders without crashing", () => {
   const rendered = renderer.create(<App />).toJSON();
--- test/CryptoList.test.js
+++ test/CryptoList.test.js
@@ -2,7 +2,6 @@ import React from "react";
 import renderer from "react-test-renderer";
 import { shallow } from 'enzyme';
 import CryptoList from "../src/components/CryptoList";
-import "./helpers/testSetup";
 
 it("renders without crashing", () => {
   const rendered = renderer.create(<CryptoList />).toJSON();
