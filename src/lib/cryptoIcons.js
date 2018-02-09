49f42b7f745886ec62820a838be117ba71d42338
johnpmitsch <johnpmitsch@gmail.com>
2018-02-08 22:44:36 -0500

    Added CryptoIcons back

---
+++ src/lib/cryptoIcons.js
@@ -0,0 +1,12 @@
+const CryptoIcons = {
+  bitcoin: require("../assets/crypto-icons/btc.jpg"),
+  litecoin: require("../assets/crypto-icons/ltc.jpg"),
+  ethereum: require("../assets/crypto-icons/eth.jpg"),
+  bitcoin_cash: require("../assets/crypto-icons/btcc.jpg"),
+  dogecoin: require("../assets/crypto-icons/doge.jpg"),
+  dash: require("../assets/crypto-icons/dash.jpg"),
+  neo: require("../assets/crypto-icons/neo.jpg"),
+  other: require("../assets/crypto-icons/question_mark.jpg")
+};
+
+export default CryptoIcons;
