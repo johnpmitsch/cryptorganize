e0ed0370c8d9e0124ae74bc0a434c288f919a013
johnpmitsch <johnpmitsch@gmail.com>
2018-02-24 17:30:14 -0500

    Added AppComponent

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { StatusBar, View, Keyboard } from "react-native";
+import { AppLoading, Asset, Font } from 'expo';
 import CryptoList from "./CryptoList";
 import styles from "../styles/styles";
 import GlobalHelpers from "../lib/GlobalHelpers";
@@ -12,12 +13,41 @@ export default class Home extends React.Component {
     headerTitleStyle: styles.headerTitle
   };
 
+  state = {
+    isLoadingComplete: false,
+  };
+
+  _loadResourcesAsync = async () => {
+    // add assets that need to be preloaded
+    return Promise.all([]);
+  };
+
+  _handleLoadingError = error => {
+    // In this case, you might want to report the error to your error
+    // reporting service, for example Sentry
+    console.warn(error);
+  };
+
+  _handleFinishLoading = () => {
+    this.setState({ isLoadingComplete: true });
+  };
+
   render() {
-    return (
-      <View style={styles.basicContainer}>
-        <StatusBar barStyle="light-content" backgroundColor="black" />
-        <CryptoList navigation={this.props.navigation} />
-      </View>
-    );
+    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+      return (
+        <AppLoading
+          startAsync={this._loadResourcesAsync}
+          onError={this._handleLoadingError}
+          onFinish={this._handleFinishLoading}
+        />
+      );
+    } else {
+      return (
+        <View style={styles.basicContainer}>
+          <StatusBar barStyle="light-content" backgroundColor="black" />
+          <CryptoList navigation={this.props.navigation} />
+        </View>
+      );
+    }
   }
 }
